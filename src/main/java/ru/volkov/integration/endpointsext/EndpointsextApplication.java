9509adbcba6bd376f020d314b210391805d9847a
evgeniy volkov <volkov.e.n@gmail.com>
2021-04-04 14:47:27 +0300

    init commit

---
+++ src/main/java/ru/volkov/integration/endpointsext/EndpointsextApplication.java
@@ -0,0 +1,13 @@
+package ru.volkov.integration.endpointsext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointsextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndpointsextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/volkov/integration/endpointsext/EndpointsextApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volkov.integration.endpointsext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointsextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
